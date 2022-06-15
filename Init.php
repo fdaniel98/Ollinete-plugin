aaace577adffd63d29e339cdaae65971b9a15d7c
Felix Licien <felix.licien@elaniin.com>
2022-06-14 20:40:25 -0400

    ollinete

---
+++ Init.php
@@ -0,0 +1,15 @@
+<?php
+namespace FacturaScripts\Plugins\Ollinete;
+
+class Init extends \FacturaScripts\Core\Base\InitClass
+{
+    public function init()
+    {
+        // se ejecuta cada vez que carga FacturaScripts (si este plugin está activado).
+    }
+
+    public function update()
+    {
+        // se ejecuta cada vez que se instala o actualiza el plugin.
+    }
+}
