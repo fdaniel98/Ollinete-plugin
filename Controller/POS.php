54ebbccca9b890ec26d8792a708cb3ce08aed77e
Felix Licien <felix.licien@elaniin.com>
2022-06-28 18:34:19 -0400

    change references to facturacli

--- Controller/POS.php
+++ Controller/POS.php
@@ -3,6 +3,8 @@
 namespace FacturaScripts\Plugins\Ollinete\Controller;
 
 use FacturaScripts\Core\Model\Cliente;
+use FacturaScripts\Plugins\POS\Lib\POS\Sales\Order;
+use FacturaScripts\Plugins\POS\Lib\POS\Sales\OrderRequest;
 
 class POS extends \FacturaScripts\Plugins\POS\Controller\POS
 {
@@ -15,6 +17,27 @@ class POS extends \FacturaScripts\Plugins\POS\Controller\POS
         $cliente = new Cliente();
         $cliente->loadFromCode($code);
         return $cliente->ref;
+    }
+
+    /**
+     * Process sales.
+     *
+     * @return void
+     */
+    protected function saveOrder()
+    {
+        if (false === $this->validateSaveRequest($this->request)) {
+            return;
+        }
+
+        $orderRequest = new OrderRequest($this->request);
+        $order = new Order($orderRequest);
+        $order->reference = $this->request->get('reference');
+
+        if ($order->save()) {
+            $this->session->storeTransaction($order);
+            $this->printTicket($order->getDocument());
+        }
     }
 
 }
