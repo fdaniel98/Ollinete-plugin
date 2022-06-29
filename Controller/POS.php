82ee817ea934de06f9a33567b99bc1815660982f
Felix Licien <felix.licien@elaniin.com>
2022-06-29 18:07:51 -0400

    ref

--- Controller/POS.php
+++ Controller/POS.php
@@ -37,28 +37,11 @@ class POS extends \FacturaScripts\Plugins\POS\Controller\POS
 
         $orderRequest = new OrderRequest($this->request);
         $order = new Order($orderRequest);
-        $order->reference = $this->request->get('reference');
+        $order->reference = $this->request->get('observaciones');
 
         if ($order->save()) {
             $this->session->storeTransaction($order);
             $this->printTicket($order->getDocument());
         }
     }
-
-    protected function holdOrder()
-    {
-        if (false === $this->validateSaveRequest($this->request)) {
-            return;
-        }
-
-        $this->request->request->set('tipo-documento', self::PAUSED_TRANSACTION);
-        $request = new OrderRequest($this->request);
-        $transaction = new Order($request);
-        $transaction->reference = $this->request->get('reference');
-
-        if ($transaction->hold()) {
-            $this->toolBox()->i18nLog()->info('operation-is-paused');
-        }
-    }
-
 }
